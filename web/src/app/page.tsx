47bdc4be5d6f52a7b7795f3c065ce6e3b5d0618d
Ethan Bickel <notethanbickel@gmail.com>
2023-11-04 19:45:46 -0500

    added auth

--- web/src/app/page.tsx
+++ web/src/app/page.tsx
@@ -1,6 +1,12 @@
+"use client";
+import { useUser } from "@auth0/nextjs-auth0/client";
 import Link from "next/link";
+import { useRouter } from "next/navigation";
 
 export default function HomePage() {
+  const user = useUser();
+  const router = useRouter();
+  if (user.user) router.push("/app/budget");
   return (
     <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c] text-white">
       <div className="container flex flex-col items-center justify-center gap-12 px-4 py-16 ">
@@ -8,7 +14,9 @@ export default function HomePage() {
           manage<span className="text-[hsl(280,100%,70%)]">fy</span>
         </h1>
         <div>Solving your finance problems with the power of AI</div>
-        <div className="rounded-md bg-blue-500 p-2">Sign in</div>
+        <Link className="rounded-md bg-blue-500 p-2" href={"/api/auth/login"}>
+          Sign in
+        </Link>
       </div>
     </main>
   );
--- web/src/components/sidebar.tsx
+++ web/src/components/sidebar.tsx
@@ -2,11 +2,14 @@ import Link from "next/link";
 
 const Sidebar = () => {
   return (
-    <div className="flex h-full min-h-screen w-1/5 flex-col items-center space-y-5 bg-neutral-secondary pt-5 text-center text-highlight">
+    <div className="flex h-full min-h-screen w-1/5 flex-col items-center bg-neutral-secondary py-5 text-center text-highlight">
       <h1 className="font-bold">manageFY</h1>
       <div className="flex flex-col space-y-2">
         <Link href="/app/budget">budget</Link>
         <Link href="/app/settings">Settings</Link>
+      </div>
+      <div className="mt-auto">
+        <Link href={"/api/auth/logout"}>signout</Link>
       </div>
     </div>
   );
