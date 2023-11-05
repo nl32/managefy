64408c620edc28c6770fefa83972f254baa1e937
Ethan Bickel <notethanbickel@gmail.com>
2023-11-05 08:12:24 -0600

    formatting

--- app.py
+++ app.py
@@ -10,15 +10,18 @@ app = Flask(__name__)
 
 # --------------------------------------------------------------------------HOME
 
+
 @app.route('/')
 def home():
-    return render_template('home.html', name = "Ethan")
+    return render_template('home.html', name="Ethan")
 
 # --------------------------------------------------------------------------LIST
+
 
 @app.route('/list')
 def list():
     return render_template('list.html')
+
 
 @app.route('/list', methods=['POST'])
 def list_post():
@@ -27,17 +30,19 @@ def list_post():
     global_dict = maker(item, price)
     print("Rout List: ")
     print(global_dict)
-    with open("data.json","w",encoding="utf8") as json_file:
-        json.dump(global_dict,json_file,ensure_ascii=True)
+    with open("data.json", "w", encoding="utf8") as json_file:
+        json.dump(global_dict, json_file, ensure_ascii=True)
     return global_dict
 
 # --------------------------------------------------------------------------DASHBOARD
 
+
 @app.route('/dashboard')
 def about():
-    return render_template('dashboard.html', name = "Ethan")
+    return render_template('dashboard.html', name="Ethan")
 
 # --------------------------------------------------------------------------LOGIN
+
 
 @app.route('/login')
 def login():
@@ -45,13 +50,15 @@ def login():
 
 # --------------------------------------------------------------------------FUTURE FINANCE
 
+
 @app.route('/futurefinance')
-def futurefinance():  
-    with open("data.json","r",encoding="utf8") as json_file:
+def futurefinance():
+    with open("data.json", "r", encoding="utf8") as json_file:
         global_dict = json.load(json_file)
     return render_template('futurefinance.html', data=global_dict)
 
 # --------------------------------------------------------------------------CHATBOT FUNCTION
+
 
 @app.route("/chat")
 def chat():
@@ -69,14 +76,10 @@ def chat_with_bot(message):
     response = openai.Completion.create(
         engine="text-davinci-003",
         prompt=f"Financial Bot: {message}",
-        max_tokens=100  
+        max_tokens=100
     )
     return response.choices[0].text.strip()
 
 
-
-
-
 if __name__ == '__main__':
     app.run(debug=True)
-
